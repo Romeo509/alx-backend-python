e37b3ebc476cb5886fef7124c010814c0ac84f64
Romeo509 <romeoameku503@gmail.com>
2024-06-06 18:20:10 +0000

    task1: solved- returning sum as float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains the add function.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two float numbers.
+
+    Parameters:
+    a (float): The first float number.
+    b (float): The second float number.
+
+    Returns:
+    float: The sum of the two float numbers.
+    """
+    return a + b
