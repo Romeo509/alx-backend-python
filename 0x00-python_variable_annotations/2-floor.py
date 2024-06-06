5dd4d35703d0837cc25cb0c1222a1c45650777a4
Romeo509 <romeoameku503@gmail.com>
2024-06-06 18:27:21 +0000

    task3: return the floor to the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module contains the floor function.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of the float.
+
+    Parameters:
+    n (float): The float number to floor.
+
+    Returns:
+    int: The floor of the float number.
+    """
+    return math.floor(n)
