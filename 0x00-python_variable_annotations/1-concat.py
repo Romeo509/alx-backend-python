fdcc890bd0d24fde97464691f71a81b6fab8c83a
Romeo509 <romeoameku503@gmail.com>
2024-06-06 18:23:48 +0000

    task2 solved - concatenate 2 files

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains the concat function.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings.
+
+    Parameters:
+    str1 (str): The first string.
+    str2 (str): The second string.
+
+    Returns:
+    str: The concatenated string.
+    """
+    return str1 + str2
