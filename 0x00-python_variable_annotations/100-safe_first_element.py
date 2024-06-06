e7e0d90e4f9fb5942e76b98c9c5bb07c9f04368a
Romeo509 <romeoameku503@gmail.com>
2024-06-06 19:02:40 +0000

    update

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This module contains the safe_first_element function.
+"""
+
+from typing import Sequence, Any, Union, Optional
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Return the first element of the list if it exists, otherwise return None.
+
+    Parameters:
+    lst (Sequence[Any]): The input list.
+
+    Returns:
+    Union[Any, None]: The first element of the list if it exists, otherwise None.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
