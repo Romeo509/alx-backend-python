4af165ee315816d70ec915ceec991f279e633ece
Romeo509 <romeoameku503@gmail.com>
2024-06-30 23:20:38 +0000

    First test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,18 +1,23 @@
-#!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map  # Import the function to be tested
 
 
 class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({}, ("a",), "KeyError: 'a'"),
+        ({"a": 1}, ("a", "b"), "KeyError: 'b'"),
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+    def test_access_nested_map_exception(
+        self, nested_map, path, expected_message
+    ):
+        """Test access_nested_map function raises
+         KeyError with expected message."""
+        with self.assertRaises(KeyError) as context_manager:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context_manager.exception), expected_message)
 
 
 if __name__ == "__main__":
